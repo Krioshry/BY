f0a8a8909051b577de3b8389a1e3e0bf0d123153
Krioshry <409424440@qq.com>
2020-12-30 16:36:24 +0800

    20201230

---
+++ demo/src/main/java/com/example/demo/Interceptor/MyWebConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.Interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+    // 将登录拦截器配置到容器中
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns(
+                        "/",
+                        "/index","/admin",
+                        "/admin/login",
+                        "/css/**", "/js/**",
+                        "/img/**",
+                        "/font_seop24v5vbs/**");
+    }
+}
--- demo/src/main/java/com/example/demo/controller/HelloWordController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWordController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,18 +22,27 @@ public class HelloWordController {
 
     @GetMapping
     public String index() {
-        return "index";
+        return "admin/index";
     }
 
     @PostMapping("login")
     public String login(@RequestParam String username,
                         @RequestParam String password,
-                        HttpSession session){
+                        HttpSession session,
+                        RedirectAttributes attributes){
         user user=userService.checkUser(username,password);
+
         if(user != null){
-            return "home";
+            user.setPassword(null);
+            session.setAttribute("user",user);
+            return "/admin/home";
         }
+        attributes.addFlashAttribute("message","账号或密码错误");
         return "redirect:/admin";
     }
 
+    @GetMapping("home")
+    public String home(){
+        return "/admin/home";
+    }
 }
